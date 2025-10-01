4a5e5e2f5c16d46db1250ffd5437f90c4427718b
Emma Reid <78507456+23reide@users.noreply.github.com>
2025-09-30 22:12:47 -0500

    Fixed process and address errors

--- readMessage.ts
+++ readMessage.ts
@@ -1,11 +1,11 @@
 import { client } from './utils/client'
 import contractAbi from './abi.json'
 
-const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS!
+const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS! as `0x${string}`
 
 export async function readSomething() {
   const data = await client.readContract({
-    address: contractAddress,
+    address: contractAddress as `0x${string}`,
     abi: contractAbi,
     functionName: 'getMessage',
   })
